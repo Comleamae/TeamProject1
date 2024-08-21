45cd5f258a21f59744a50fbb7aa3602238954156
Comleamae <lbcy00@gmail.com>
2024-08-21 09:17:57 +0900

    #Start

---
+++ src/test/java/com/green/TeamProject1/TeamProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.TeamProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
