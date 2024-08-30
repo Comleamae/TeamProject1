e6e1e8dbcfea6737e1f56f6dff2f4a290dd3b6ab
cyubro195 <cyubro195@gmail.com>
2024-08-30 16:41:25 +0900

    sorry 0830

--- src/main/frontend/src/pages/Main.js
+++ src/main/frontend/src/pages/Main.js
@@ -3,7 +3,7 @@ import '../reset.css'
 import './Main.css'
 import { LuCalendarClock } from "react-icons/lu";
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { BsCreditCard } from "react-icons/bs";
+import { IoCardOutline } from "react-icons/io5"; //카드 아이콘 
 import { IoNewspaperOutline } from "react-icons/io5";
 import { FaUserDoctor } from "react-icons/fa6";
 import { FaHandHoldingMedical } from "react-icons/fa";
@@ -16,7 +16,6 @@ const Main = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const hideMainRoutes = ['/user','/admin']; // 이동할때 숨김
-
   //목록있는지 확인
   const shouldHideMain = hideMainRoutes.includes(location.pathname);
   if(shouldHideMain){
@@ -40,63 +39,77 @@ const Main = () => {
 
       {/* 진료/수납결제/의료진 박스 */}
       <div className='self-box'>
+
+        {/*  첫번째 줄 */}
         <div className='sub-idex-box' onClick={(e) => { navigate('/user/clinicPrint') }}>
           <FaHandHoldingMedical className='icon' />
           <p>진료</p>
           <span>진료볼수있는곳</span>
         </div>
-        <div className='sub-idex-box' onClick={(e) => { navigate('/user/clinicPrint') }}>
+
+        <div className='sub-idex-box' onClick={(e) => { navigate('/user/reserv') }}>
           <LuCalendarClock className='icon' />
           <p>온라인<br />진료예약</p>
           <span>편하게 온라인으로 진료예약 하러가기</span>
         </div>
-        <div className='sub-idex-box' onClick={(e) => { navigate(`/admin/moneyln`) }}>
+
+        <div className='sub-idex-box' onClick={(e) => { navigate(`/`) }}>
           <FaUserDoctor className='icon' />
           <p>의료진/<br /> 의료과 찾기</p>
           <button type='button' className='btn'>찾기</button>
         </div>
-        <div className='sub-idex-box' onClick={(e) => { navigate('/user/clinicPrint') }}>
-        <GrMapLocation className='icon' />
-          <p>찾아오시는 길</p>
-          <span>제증명 발급에 대한 안내</span>
+
+        <div className='sub-idex-box' onClick={(e) => { navigate('/') }}>
+          <p>빈방</p>
+          <span>빈곳</span>
         </div>
-        <div className='sub-idex-box' onClick={(e) => { navigate(`/admin/moneyln`) }}>
-          <BsCreditCard className='icon' />
-          <p>진료비 결제</p>
-          <span>기다리지말고 온라인으로 진료비 결제하세요!</span>
-        </div>
+
+        <div className='sub-idex-box' onClick={(e) => { navigate('/') }}>
+          <p>빈방1</p>
+          <span>빈곳1</span>
+        </div> 
+
       </div>
+      
+      {/*  두번째 줄 */}
       <div className='bottom-box'>
-        <div className='sub-idex-box' onClick={(e) => { navigate('/user/clinicPrint') }}>
-          <LuCalendarClock className='icon' />
-          <p>온라인<br />진료예약</p>
-          <span>편하게 온라인으로 진료예약 하러가기</span>
+        
+        <div className='sub-idex-box' onClick={(e) => { navigate('/') }}>
+          <GrMapLocation className='icon' />
+          <p>찾아오시는 길</p>
+          <span>병원에 찾아오는 길입니당</span>
         </div>
-        <div className='sub-idex-box' onClick={(e) => { navigate(`/admin/moneyln`) }}>
-          <BsCreditCard className='icon' />
+
+        <div className='sub-idex-box' onClick={(e) => { navigate('/user/moneyIn') }}>
+          <IoCardOutline className='icon' />
           <p>진료비 결제</p>
-          <span>기다리지말고 온라인으로 진료비 결제하세요!</span>
+          <span>
+            이제 기다리지말고<br />
+            온라인으로 진료비 결제하세요!
+          </span>
         </div>
-        <div className='sub-idex-box' onClick={(e) => { navigate('/user/clinicPrint') }}>
+
+        <div className='sub-idex-box' onClick={(e) => { navigate('/') }}>
+          <p>99</p>
+          <span>99</span>
+        </div>
+
+        <div className='sub-idex-box' onClick={(e) => { navigate('/') }}>
+          <p>88</p>
+          <span>88</span>
+        </div>
+
+        <div className='sub-idex-box' onClick={(e) => { navigate('/admin/moneyln') }}>
           <IoNewspaperOutline className='icon' />
           <p>증명서 발급</p>
           <span>제증명 발급에 대한 안내</span>
         </div>
+
         <div className='sub-idex-box' onClick={(e) => { navigate('/user/clinicPrint') }}>
-          <LuCalendarClock className='icon' />
-          <p>온라인<br />진료예약</p>
-          <span>편하게 온라인으로 진료예약 하러가기</span>
+          <p>10</p>
+          <span>00</span>
         </div>
-        <div className='sub-idex-box' onClick={(e) => { navigate(`/admin/moneyln`) }}>
-          <BsCreditCard className='icon' />
-          <p>진료비 결제</p>
-          <span>기다리지말고 온라인으로 진료비 결제하세요!</span>
-        </div>
-        <div className='sub-idex-box' onClick={(e) => { navigate('/user/clinicPrint') }}>
-          <IoNewspaperOutline className='icon' />
-          <p>증명서 발급</p>
-          <span>제증명 발급에 대한 안내</span>
-        </div>
+
       </div>
       <div className='background-div'></div>
 
--- src/main/frontend/src/pages/user/kth/Join.js
+++ src/main/frontend/src/pages/user/kth/Join.js
@@ -4,13 +4,9 @@ import { useDaumPostcodePopup } from 'react-daum-postcode';
 import { useNavigate } from 'react-router-dom'
 import { joinValidate } from './joinValidate';
 import axios from 'axios';
-<<<<<<< HEAD
-=======
-
-
 //import { Modal } from 'bootstrap';
 
->>>>>>> cyh
+
 const Join = () => {
 
   const navigate = useNavigate()
