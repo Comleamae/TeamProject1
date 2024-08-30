b2f6569e9bf458309deb2dd3adfbab0002fd2f36
Latee0504 <rose98765427@gmail.com>
2024-08-30 15:31:17 +0900

    0830 331

--- src/main/frontend/src/pages/user/kth/Login.js
+++ src/main/frontend/src/pages/user/kth/Login.js
@@ -2,16 +2,16 @@ import React, { useState } from 'react'
 import './Login.css'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-
-const Login = ({setLoginInfo}) => {
-
+const Login = ({ setLoginInfo }) => {
   const navigate = useNavigate()
-
   //로그인 시 입력한 정보
   const [loginData, setLoginData] = useState({
     memId: '',
     memPw: ''
   });
+
+
+
 
   //입력한 정보로 바꾸기
   function changeLoginData(e) {
@@ -21,18 +21,32 @@ const Login = ({setLoginInfo}) => {
     }
     setLoginData(newData)
   }
-  
+
   //로그인 버튼 클릭시 실행
   function login() {
     axios.post('/api_member/login', loginData)
       .then((res) => {
         // 로그인 가능 시
-        if (res.data != '') { 
+        if (res.data != '') {
           alert('환영합니다.')
+          //sessionStorage에 로그인한 회원의 개인정보 저장
           const loginInfo = {
             memId: res.data.memId,
-            memPw: res.data.memPw
+            memPW: res.data.memPw,
+            memName: res.data.memName,
+            memTel: res.data.memTel,
+            citizenNum: res.data.citizenNum,
+            memAddr: res.data.memAddr,
+            addrDetail: res.data.addrDetail,
+            memEmail: res.data.memEmail,
+            post: res.data.post,
+            memRole: res.data.memRole
           }
+          const json_loginInfo = JSON.stringify(loginInfo);
+
+          window.sessionStorage.setItem('loginInfo', json_loginInfo);
+          navigate('/')
+          setLoginInfo(loginInfo)
           window.sessionStorage.setItem('loginInfo', JSON.stringify(loginInfo));
         }
         else {
@@ -43,7 +57,6 @@ const Login = ({setLoginInfo}) => {
         console.log(error)
       })
   }
-
   return (
     <div className='login-container'>
       <div><h3>로그인</h3></div>
@@ -51,6 +64,7 @@ const Login = ({setLoginInfo}) => {
         <div className='login-div'>
           <div><input type='text' name='memId' placeholder='아이디' onChange={(e) => { changeLoginData(e) }}></input></div>
           <div><input type='password' name='memPw' placeholder='비밀번호' onChange={(e) => { changeLoginData(e) }}></input></div>
+          <button type='button' className='btn-div' onClick={(e) => { navigate('/user/join') }}>회원가입</button>
           <button type='button' className='btn-div' onClick={(e) => { navigate('/join') }}>회원가입</button>
         </div>
         <button type='button' className='btn-div' onClick={(e) => { login() }}> 로그인</button>
@@ -58,5 +72,4 @@ const Login = ({setLoginInfo}) => {
     </div>
   )
 }
-
 export default Login
