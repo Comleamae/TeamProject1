48b9610b10d848d5b4af7d0867c027c50728b509
Latee0504 <rose98765427@gmail.com>
2024-09-02 09:19:04 +0900

    0902

--- src/main/frontend/src/pages/user/ksh/NewVisit.js
+++ src/main/frontend/src/pages/user/ksh/NewVisit.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 const NewVisit = () => {
   return (
-<<<<<<< HEAD
-    <div className='newVisit'>33333333332332233232323
-=======
-    <div>
-      <h3>123123123123</h3>
+    <div className='newVisit'>
 
->>>>>>> kth
+      <div>
+        <h3>123123123123</h3>
+      </div>
     </div>
   )
 }
