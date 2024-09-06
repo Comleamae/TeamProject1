2946f4c3ea635a4a4b6cec74e875656226ba78b6
Comleamae <lbcy00@gmail.com>
2024-09-06 12:12:15 +0900

    123

--- src/main/frontend/src/pages/admin/pjw/AdminMain.js
+++ src/main/frontend/src/pages/admin/pjw/AdminMain.js
@@ -2,11 +2,16 @@ import React from 'react'
 import './AdminMain.css'
 //관리자 화면의 메인
 const AdminMain = () => {
-  
+
   return (
-    <div className='main'>
-      <div className='in-div'>
-        여기가 메인
+    <div className='admin-main'>
+      여기가 메인
+      <div className='div-in'>
+        
+        <div className='div-function'>
+          <div>문진표 작성</div>
+          <div>의료진 진료 스케줄 확인</div>       
+        </div>
       </div>
     </div>
   )
